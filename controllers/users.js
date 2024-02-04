bff146496dc971ccada118a0532bc1d23846d6b9
sanele23 <sanele23@live.com>
2024-02-04 20:32:37 +0200

    update

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,13 @@ const mongodb = require("../data/database"); // mongodb connection
 // a variable that mongo will use to create uniqui identifier for each database
 const ObjectId = require("mongodb").ObjectId;
 
-const getAll = async (req, res) => {};
+const getAll = async (req, res) => {
+  const result = await mongodb.getDatabase().db().collection("users").find();
+  result.toArray().then((users) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(users);
+  });
+};
 
 const getSingle = async (req, res) => {
   const userId = new ObjectId(req.params.id);
